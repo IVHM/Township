e767d75364039729720ee3ec353490af2ea3e1d9
Von_h <hmatthyseniv@gmail.com>
2019-09-25 21:05:15 -0500

    Add: Stone breaking animation

--- Stone001.gd
+++ Stone001.gd
@@ -1,6 +1,7 @@
 extends Sprite
 
 export var type = "interactable object"
+var alive = true setget get_alive
 export var health = 10
 export var armor = 5
 export var inventory = { "stone" : 15}
@@ -11,18 +12,25 @@ var total = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	total = 0
+	self.total = 0
 	for i in inventory.values()
-		total += i
+		self.total += i
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if self.alive && self.transition:
 
 
 func take_hit(dmg):
 	dmg -= armor 
 	if dmg > 0:
-		
+		health -= dmg 
+	else:
+		print("Your tool is not strong enough")
+	
+	if health <= 0:
+		alive = false
+
+getset 
