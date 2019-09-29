1ffd12a573325afd5bad317b8a2c7eda71a68b35
Von_h <hmatthyseniv@gmail.com>
2019-09-28 20:42:08 -0500

    Mov: Moved input control into the Player Node
    MousePath2D is now the root node of the Player Node

--- Objects/Player.gd
+++ Objects/Player.gd
@@ -1,13 +1,15 @@
-extends PathFollow2D
+extends Path2D
 
 export var type = "player"
 
 # MOVEMENT VARIABLES
 var moving = true
 export var speed = 40
+onready var follower = $PlayerFollower
 
 #ANIMATION VARIABLES
 var crnt_state = 0
+onready var sprite = $PlayerFollower/AnimatedSprite
 export var anim = ["Idle", "Walking","Talking","Waving"]
 export var repeat_chance = .6
 
@@ -17,6 +19,8 @@ export var health = 100
 export var inventory = {'gold': 100, 'wood': 100, 'stone': 50}
 
 # INTERACTION VARIABLES
+signal player_clicked
+onready var interaction_timer = $InteractionTimer 
 var near_resource = false
 var resource = null
 onready var mining_timer = $MiningTimer
@@ -25,88 +29,108 @@ var transition = false
 var showing = true  # Are we displaying this peasent yet?
 var rnd = RandomNumberGenerator.new()
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-		$AnimatedSprite.set_animation("Idle")
-		$AnimatedSprite.play()
+		sprite.set_animation("Idle")
+		sprite.play()
 		crnt_state = 0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 
 	if crnt_state == 1:  # Walking
-		if self.unit_offset >= .99:
+		if follower.unit_offset > .99:
 			crnt_state = 0
-			unit_offset = .99
-			$AnimatedSprite.set_animation(anim[crnt_state])
-			
+			sprite.set_animation(anim[crnt_state])
 		else:
-			self.set_offset( self.get_offset() + (speed*delta))
+			follower.set_offset( follower.get_offset() + (speed*delta))
 		
 
 # function to change animation state externally
 func change_state(state):
 	if crnt_state != state:
 		crnt_state = state
-		$AnimatedSprite.set_animation(anim[crnt_state])	
+		sprite.set_animation(anim[crnt_state])	
 
 
-# Returns PathFollower posito the begining of the 2DPath
+# Returns PathFollower position to the begining of the 2DPath
 func reset_pos():
-	self.unit_offset = 0
+	self.follower.unit_offset = 0
 
 
 # Handles interactions with other 2DAreas
-func _on_Area2D_area_entered(body):
-	
-
-	if body.type == "harvestable object":
-		print("near stone", body)
+func _on_PlayerArea2D_area_entered(body):
+	print("Area2D collision detected ")
+	if !transition:
 		if body.alive:
-			self.near_resource = true
-			resource = body
-			mine(body)
-
+			if body.type == "harvestable object":
+				print("near stone", body)
+				self.near_resource = true
+				resource = body
+				#face_interaction(body.position)
+				mine(body)
 	
 	var a = false # Functionality currently disabled
+				  # needs to be converted once
 	if a:
 		print("collision detected (area)")
-		if !transition:
-			if body.type == "Interaction":
-				crnt_state = rnd.randi_range(2,3)
-				$AnimatedSprite.set_animation(anim[crnt_state])
-				$InteractionTimer.start()
-			
-			if body.type == "Intersection":
-				print("Intersection entered")
-				var overlaping_areas = body.get_overlapping_areas()
-				print("area overlapped array", overlaping_areas) 
-				for i in overlaping_areas:
-					print(i, i.type, i.position)
+	
+		if body.type == "Interaction":
+			crnt_state = rnd.randi_range(2,3)
+			sprite.set_animation(anim[crnt_state])
+			$InteractionTimer.start()
+		
+		if body.type == "Intersection":
+			print("Intersection entered")
+			var overlaping_areas = body.get_overlapping_areas()
+			print("area overlapped array", overlaping_areas) 
+			for i in overlaping_areas:
+				print(i, i.type, i.position)
 
 
-func _on_Area2D_area_exited(body):
+func _on_PlayerArea2D_area_exited(body):
 	transition = false
 	if body.type == "harvestable object":
 		print("left stone", body)
 		self.near_resource = false
+		self.resource = null
 
-# Buffer for collision detection and animation selection
-func _on_InteractionTimer_timeout():
+
+func face_interaction(pos):
+	print("Moving to face resource")
+	var new_curve = Curve2D.new()
+	new_curve.add_point(follower.position)
+	new_curve.add_point(pos)
+	self.set_curve(new_curve)
+	reset_pos()
+
+
+func start_transition():
 	transition = true
-	var repeats = randi()
+	interaction_timer.start()
+
 	
-	crnt_state = 1
-	$AnimatedSprite.set_animation(anim[crnt_state])
+# Buffer for collision detection and animation selection so collision don't double bounce
+func _on_InteractionTimer_timeout():
+	transition = false
+
 
 func mine(body):
+	print("swinging pickaxe at ", body.type, body.alive)
 	body.take_hit(10, self)
+	print(body.alive)
 	if body.alive:
 		mining_timer.start(mining_timer_len)
+		#mining_timer.start()
+		print("started mining timer")
 	else:
 		near_resource = false
 
+
 func _on_MiningTimer_timeout():
+	print("MiningTimer Done")
 	if near_resource:
 		mine(resource)
 			
@@ -118,3 +142,34 @@ func transfer(inventory_in):
 			inventory[i] += inventory_in[i]
 		else:
 			inventory[i] = inventory_in[i]
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		var new_end_point = get_global_mouse_position() 
+		var new_start_point = follower.position
+		print("new_end_point", new_end_point, " : new_start_point", new_start_point)
+	
+		if near_resource: # This stops the area2d from reacting 
+			start_transition()
+	
+		emit_signal("player_clicked", new_start_point, new_end_point, self)
+
+
+func update_path(new_path):
+	print(follower.position)
+	var new_curve = Curve2D.new()
+	var pos = follower.position
+	new_curve.add_point(pos)
+
+	for i in new_path:
+		new_curve.add_point(i)
+	
+	self.set_curve(new_curve)
+	print("set curve")
+	reset_pos()
+	print("reset position")
+	change_state(1)
+	print(follower.position)
+
+
--- TESTS/Material interaction/StoneTest.gd
+++ TESTS/Material interaction/StoneTest.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
 onready var nav_tiles = $TileNavigation
-onready var cur_path = $MousePath
-onready var pc = $MousePath/Player
+onready var pc = $Player
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pc.connect("player_clicked", self, "_on_player_clicked")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -18,18 +17,6 @@ func _ready():
 func _on_player_clicked(start, end, player):
 	print("\n\n----\n--\n--\nCreating path")
 	var new_path = nav_tiles.get_simple_path(start,end)
-	var new_curve = Curve2D.new()
+	player.update_path(new_path)
 
-	var pos = pc.position
-	new_curve.add_point(pc.position)
 
-	for i in new_path:
-		new_curve.add_point(i)
-	
-	cur_path.set_curve(new_curve)
-	pc.reset_pos()
-	pc.change_state(1)
-	
-#	cur_path.set_curve(new_curve)
-#	pc.reset_pos()
-#	pc.change_state(1)
