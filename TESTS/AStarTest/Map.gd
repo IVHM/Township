960a519c91609742f7df33c2ddeb2c3e47b36c1c
Hendrik M <hmatthyseniv@gmail.com>
2019-10-19 15:07:00 -0500

    Add : Astar point connection

--- TESTS/AStarTest/Map.gd
+++ TESTS/AStarTest/Map.gd
@@ -32,14 +32,18 @@ export var road_tile_truth_table = { 0: 0,
 #
 # ASTAR VARIABLES
 var astar = AStar.new()
-export var tile_weights = {"Grass": 4,
-						   "Road" : 1}
+var astar_id_lookup = {}
+export var tile_weights = {"Grass" : 4,
+						   "Road"  : 1,
+							"Tile" : 8}
 
+##
 # Random numbers
 var rnd = RandomNumberGenerator.new()
 
 
 #TESTING GENERATION VARIABLES
+export var generating = false
 var generated = false
 onready var testing_timer = $Timer
 
@@ -55,26 +59,26 @@ func _ready():
 func load_tile_ids():
 	self.tile_set = world_tilemap.get_tileset()
 	self.tile_ids_amt = len(tile_set.get_tiles_ids())
-	print("###################")
-	print("###TILE IDS####")
-	print(self.tile_ids_amt)
-	print("###################")
+	
 	for i in range(tile_ids_amt):
 		self.tile_id_name_lookup.append(tile_set.tile_get_name(i))
-		print(i,":", self.tile_id_name_lookup[i])
+		#print(i,":", self.tile_id_name_lookup[i])
 
-	print("###################")
 #
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if first_run:
 		load_tile_ids()
 		first_run = false
-
-	if !generated:
 		generate_random_map()
-		testing_timer.start()
-		generated = true
+
+		
+
+	if generating:
+		if !generated:
+			generate_random_map()
+			testing_timer.start()
+			generated = true
 		
 
 func _on_Timer_timeout():
@@ -85,10 +89,26 @@ func generate_random_map():
 	for i in range(size.x):
 		for j in range(size.y):
 			world_tilemap.set_cell(i, j, rnd.randi_range(0, len(tile_id_name_lookup) - 1))
-			var tile_type = tile_id_name_lookup[world_tilemap.get_cell(i,j)].rsplit('_')[0]
+			var tile_type = tile_id_name_lookup[world_tilemap.get_cell(i,j)].rsplit('_')
 			print(tile_type)
+			tile_type = tile_type[0]
 			var astar_id = i*size.y + j
 			var tile_coords = world_tilemap.map_to_world(Vector2(i,j))
+			self.astar_id_lookup[astar_id] = Vector2(i,j)
 			tile_coords = Vector3(tile_coords.x, tile_coords.y, 0)
 			astar.add_point(astar_id, tile_coords, tile_weights[tile_type] )
+	initialize_connections()
+
+func initialize_connections():
+	for crnt_id in self.astar_id_lookup:
+		var neighbors = [crnt_id - size.y,  # Top
+					 	 crnt_id + 1,       # Right
+					 	 crnt_id + size.y,  # Bottom
+					  	 crnt_id - 1        # Left
+					]
 		
+		for crnt_neighbor in neighbors:
+			if crnt_neighbor<0 || crnt_neighbor> size.y:
+				pass
+			else:
+				astar.connect_points(crnt_id, crnt_neighbor)
