b83d950e2c95202c0f92bfe3c8207f160528cb68
Hendrik M <hmatthyseniv@gmail.com>
2019-11-05 14:27:40 -0600

    Add: Globals database for cross scene reference

---
+++ GLOBALS.gd
@@ -0,0 +1,23 @@
+extends Node
+
+# Used to determine the bit flags of an interaction menu
+const  INTERACTION_TYPES = {
+			"Null" = 1,
+			"Talk" = 2,
+			"Trade" = 4,
+			"Job" = 8,
+			"Cancel" = 16,
+			"Mine" = 32,
+			"Chop" = 64,
+			"Open" = 128
+						   }
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
