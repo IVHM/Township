91612530b43db4cbf0b236f264929417a0426e54
Hendrik M <hmatthyseniv@gmail.com>
2019-11-14 13:37:43 -0600

    Add: GLobals for map size and dif scene types

--- GLOBALS.gd
+++ GLOBALS.gd
@@ -1,23 +1,24 @@
 extends Node
 
 # Used to determine the bit flags of an interaction menu
-const  INTERACTION_TYPES = {
-			"Null" = 1,
-			"Talk" = 2,
-			"Trade" = 4,
-			"Job" = 8,
-			"Cancel" = 16,
-			"Mine" = 32,
-			"Chop" = 64,
-			"Open" = 128
+# All corresponding png resources must be named exactly the same
+export var INTERACTION_TYPES = {
+			"Null"   : 1,
+			"Talk"   : 2,
+			"Trade"  : 4,
+			"Job"    : 8,
+			"Cancel" : 16,
+			"Mine"   : 32,
+			"Chop"   : 64,
+			"Open"   : 128,
+			"Place"  : 356
 						   }
 
+export var MENU_TYPES = ["Interactions"]
+# Used to generate all the different objects on the game map						   
+export var OBJECT_TYPES = ["Barrel"]
 
+#### MAP VARIABLES
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+export var MAP_SIZE = Vector2(30,30)
+export var TILE_SIZE = Vector2(16,16)
