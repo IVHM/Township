95546aff95a4f41a3a16ce6258445c9da5e35d74
Hendrik M <hmatthyseniv@gmail.com>
2019-11-19 21:09:23 -0600

    Add: general toggle for frameinfo

--- Scenes/MainControlHub/FrameInformationTimer.gd
+++ Scenes/MainControlHub/FrameInformationTimer.gd
@@ -1,5 +1,5 @@
 extends Timer
-
+export (bool) var display_on = false
 export (int, FLAGS, "FPS", "Static Mem", "Dynamic Mem", "Object Cnt", "Texture Mem") var output_flags = 0
 var NUM_FLAGS = 5
 var MB = 1000000
@@ -8,19 +8,20 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass	
 
 
 func _on_FrameInformationTimer_timeout():
-	if output_flags & 16:
-		print("Frames per sec   : ", Performance.get_monitor(Performance.TIME_FPS))
-	if output_flags & 1:
-		print("Texture Mem Used : ", int(Performance.get_monitor(Performance.RENDER_TEXTURE_MEM_USED)) / MB, "mb")
-	if output_flags & 2:
-		print("Num of objects   : ", Performance.get_monitor(Performance.OBJECT_COUNT))
-	if output_flags & 4:
-		print("Dynamic Mem Used : ", int(Performance.get_monitor(Performance.MEMORY_DYNAMIC)) / MB, "mb")	
-	if output_flags & 8:
-		print("Static Mem Used  : ", int(Performance.get_monitor(Performance.MEMORY_STATIC)) / MB, "mb")
+	if display_on:
+		if output_flags & 16:
+			print("Frames per sec   : ", Performance.get_monitor(Performance.TIME_FPS))
+		if output_flags & 1:
+			print("Texture Mem Used : ", int(Performance.get_monitor(Performance.RENDER_TEXTURE_MEM_USED)) / MB, "mb")
+		if output_flags & 2:
+			print("Num of objects   : ", Performance.get_monitor(Performance.OBJECT_COUNT))
+		if output_flags & 4:
+			print("Dynamic Mem Used : ", int(Performance.get_monitor(Performance.MEMORY_DYNAMIC)) / MB, "mb")	
+		if output_flags & 8:
+			print("Static Mem Used  : ", int(Performance.get_monitor(Performance.MEMORY_STATIC)) / MB, "mb")
 	
