bd823a260e3fc5eb2f6b413efd023f4bb6410913
Hendrik M <hmatthyseniv@gmail.com>
2019-11-21 20:06:14 -0600

    Add: Function for container inventory manipulation

--- Scenes/Objects/MapObjects/Container.gd
+++ Scenes/Objects/MapObjects/Container.gd
@@ -1,11 +1,27 @@
-extends "res://Objects/MapObjects/MapObject_template.gd"
+extends "res://Scenes/Objects/MapObjects/MapObject_template.gd"
 
 export var inventory = {}
 export var max_capacity = 10
 export var crnt_capacity = 0
 
+
+
 func _ready():
 	print(position, "ready barrel" )
+##
+# Takes in a dictionary of items and their amounts, use negative amount to take items
+func change_items_amt(items_in): 
+	for i in items_in:
+		if i in inventory.keys():
+			inventory[i] += items_in[i]
+			if inventory[i] <= 0:
+				inventory.erase(i)
+		else:
+			inventory[i] = items_in[i]
+			if inventory[i] <= 0:
+				print("You tried to add a negative amount of stuff to an inventory")
+				print("Check your code bub.")
+				#print("asl"+true)
 
-func add_item(): 
-	pass
+func get_inventory():
+	return inventory
