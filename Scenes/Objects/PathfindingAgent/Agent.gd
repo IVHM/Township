0c1ce8a2ae64552a3560f60dd00ed0062154fd60
Hendrik M <hmatthyseniv@gmail.com>
2019-11-29 18:59:15 -0600

    Upd: Work for remote bug test on pathfinding

---
+++ Scenes/Objects/PathfindingAgent/Agent.gd
@@ -0,0 +1,120 @@
+extends KinematicBody2D
+
+#export (NodePath) var Sprite
+
+
+var moving = false
+export (int) var speed = 40
+
+# PATH VARIABLES
+var current_path     # PoolVector2Array of point that correspond to the points on an astar path
+var calculated_path = PoolVector2Array() # Path used while testing if the path is clear
+var requested_path   # Used to hold requested paths before processing
+var path_clear = false
+var crnt_point = 0
+var next_point = 1
+var heading
+
+
+#### TESTING
+export var testing = false
+export var t_print = false 
+
+export (NodePath) var test_line 
+func _ready():
+	pass
+
+func _process(delta):
+		
+	if moving:
+		if next_point == len(current_path):  # Check if we've reached the end of the path
+			moving = false
+		else:
+			var heading = current_path[crnt_point] - current_path[next_point]
+			var dist_to_next = heading.length() 
+			var movement_vec = heading.normalized() * speed
+			if movement_vec.length() > dist_to_next:
+				movement_vec = heading 
+				crnt_point += 1
+				next_point += 1
+			
+			self.set_global_rotation(heading.get_angle())
+			self.move_and_slide(movement_vec)
+
+##
+# Emits a signal requesting a path update from the control hub
+func path_request(start, end):
+	EVENTS.emit_signal("path_request", start, end, self)
+
+
+func create_path(new_path):
+	update_path(new_path)
+	check_path(new_path[0], new_path[-1])
+	print("while ran for a while", calculated_path)
+##
+# takes in the requested path and stores it 
+func update_path(new_path):
+	requested_path = new_path
+	if t_print: print("Requested path updated: \n", requested_path)
+##
+# Goes through the path and makes sure there are no collisions a 
+func check_path(start, end):
+	var safe_point = start  # Sets the begining of the path
+	
+	### CODE TO MAKE SURE INITIAL LOCATION IS COLLISION FREE GOES HERE ###
+	print(" path checking started")
+	var while_cnt = 0
+	# Run through the path until we've reached the last point in the path
+	while !path_clear:
+		path_request(safe_point, end)  # Request a new path from the last safe point 
+		
+		for i in range(len(requested_path)):  # Runs through each point in requested_path 
+			if collision_detect(requested_path[i], requested_path[i+1]): # And checks to see if collision occurs between it and the next in libe
+				#print("path collision detected")
+				safe_point = requested_path[i]                           # If so it sets a new safe point
+				for j in range(i+1):                                     # And adds all the already checked points to the calculated_path
+					calculated_path.append(requested_path[j])
+
+				var rel_vec = requested_path[i+1] - requested_path[i]    # Now we try to calculate a new collision free path
+				var left = safe_point + rel_vec.rotated(-PI/8)
+				var right = safe_point + rel_vec.rotated(PI/8)
+				var left_clear = collision_detect(safe_point, left)
+				var right_clear = collision_detect(safe_point, right)
+				
+				if left_clear and right_clear:   # Checks to see if any of the new paths are free
+					var dist_left = left.distance_to(end)
+					var dist_right = right.distance_to(end)
+					if dist_left < dist_right:   # If both are choose the one closest to end point
+						safe_point = left
+					else:
+						safe_point = right
+				elif left_clear:
+					safe_point = left
+				elif right_clear:
+					safe_point = right
+				else:
+					print("Sorry don't knopw what to do when both new paths collide")
+					print("I haven't writen code for this bit yet, forgive me")
+				
+				# Once we've created a new safe_point we break out of the loop and start again from new safe_point
+				break
+
+			# If there were no collisions detected and the next point is the end
+			elif requested_path[i+1] == end:
+				for c_pnt in requested_path:   # Update the calculated_path witht the new points
+					calculated_path.append(c_pnt)
+				current_path = calculated_path   # Set the current path
+				calculated_path = PoolVector2Array() # reset 
+				moving = true
+				path_clear = true
+				print("legal path created")
+		while_cnt += 1
+		if while_cnt>100:
+			print("while ran for a while", calculated_path)
+			break
+					
+##
+# Moves a virtual body between the teo given vectors and returns true if a 
+func collision_detect(start, end):
+	var rel_vec = end - start
+	return self.test_move(self.get_transform(), rel_vec)
--- Scenes/Objects/PathfindingAgent/Code testing.gd
+++ Scenes/Objects/PathfindingAgent/Code testing.gd
@@ -4,15 +4,13 @@ export (NodePath) var Follower
 export (NodePath) var StuckTimer
 
 #### MOVEMENT BEHAVIOR VARIABLES
-export var start_pos = Vector2(0,0)
+export var start_pos = Vector2(16,16)
 var moving = false
 var point_array # This holds the list of vectors that make up the curve
 var crnt_point = 0 # The index of the point we are going from
 var dest_point = 4 # the index of the next point in the curve
 
-export (int) var speed 
-var heading
-var prev_pos 
+
 
 #### Testing
 export (NodePath) var test_map
@@ -20,52 +18,30 @@ export (NodePath) var PathViz
 export var testing = false
 export var t_print = false
 export (Vector2) var test_dest = Vector2(200,200)
-
+var first_run = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Follower = get_node(Follower)
 	Follower.set_position(start_pos)
-	prev_pos = start_pos
 
-
-	####TESTING####
+	if t_print: print("intial load done")
 	if testing:
 		PathViz = get_node(PathViz)
 		test_map = get_node(test_map)
 		test_map.load_tile_ids()
 		test_map.generate_blank_map()
-		point_array = get_new_path(test_dest)
-		
-		PathViz.set_points(point_array)
-		print(point_array, "follower:", Follower)
-		heading = UTIL.calc_head(Follower.get_position(), point_array[dest_point])
-		Follower.set_global_rotation(heading.angle())
-		moving = true
-
-func get_new_path(dest):
-	if testing:
-		return test_map.get_astar_path(Follower.get_position(), dest)
-	#	EVENTS.emit_signal("path_requested", Follower.get_position(), dest, self)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if moving:
-		Follower.move_and_slide(heading * speed)
-		# Check how far we are from our crnt vs next point along the path
-		var dist_crnt = Follower.get_position().distance_to(point_array[crnt_point])
-		var dist_next = Follower.get_position().distance_to(point_array[crnt_point+1])
+	####TESTING####
+	if testing:
+		if first_run:
+			Follower.create_path(test_map.get_astar_path(Follower.get_global_position(), test_dest))
+			
+			
+			print( "follower:", Follower)
 
-		# If next grtr crnt, we increment the point counters, and adjust heading
-		if dist_next < dist_crnt:
-			crnt_point += 1
-			if dest_point < len(point_array):
-				dest_point += 1
-			heading = UTIL.calc_head(Follower.get_position(), point_array[dest_point])
-			Follower.set_global_rotation(heading.get_angle())
-
-		if cnrt_point == len(point_array) - 1:
-			moving = false
-
+			first_run = false
 #func _on_StuckTimer_timeout():
 #	pos_difference = 	
